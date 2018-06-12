25d69387769a7090b1488300e4ef74f835cf64f2
Liam Neville <lineville@vassar.edu>
2018-06-12 16:02:30 -0400

    cat started

--- bash.js
+++ bash.js
@@ -1,16 +1,26 @@
 const pwd = require('./pwd.js');
 const ls = require('./ls.js');
+const cat = require('./cat.js');
 
 // output a prompt
 process.stdout.write('prompt > ');
 
 // fires on line input
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
+  const cmd = data.toString()
+    .trim()
+    .split(' ')[0];
+
+  const args = data.toString()
+    .trim()
+    .split(' ')
+    .slice(1);
   switch (cmd) {
     case 'pwd': pwd();
     break;
     case 'ls': ls();
+    break;
+    case 'cat': cat(args);
     break;
     default: console.log('bad command');
   }
