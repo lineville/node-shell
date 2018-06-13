a0e2b77d89730b24c957b174af05fd96df530a16
Liam Neville <lineville@vassar.edu>
2018-06-13 15:53:24 -0400

    cat working

--- cat.js
+++ cat.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
-module.exports = function(fileName) {
+module.exports = (fileName) => {
   // take the filename and output to the console the contents
+  fs.readFile(fileName, 'utf8', (err, data) => {
+    if (err) {
+      throw err
+    } else {
+      process.stdout.write(data)
+      process.stdout.write('\nprompt > ');
+    }
+  })
+
 }
